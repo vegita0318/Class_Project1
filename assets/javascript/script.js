740dfa965e24c4fb8f06d035b108dfed4753e4c3
Garrion Martin <unit678@gmail.com>
2018-10-02 21:33:35 -0400

    corrected css and script link and added yelp api

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -16,3 +16,19 @@ function onSearchResponse(response) {
     var responseString = JSON.stringify(response, '', 3);
     document.getElementById('response').innerHTML = responseString;
 }
+
+//yelp code below still nneds to add phone, address ect...
+
+var settings = {
+  async: true,
+  crossDomain: true,
+  url: 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/north-india-restaurant-san-francisco',
+  method: 'GET',
+  headers: {
+    Authorization: 'Bearer x0DthcS8vY40RH1ub7L304JcXg93fjOKvrR5g2UTfto-iAodtfYFt5wN43sWmC_z23Gg-PcBHld_XSNiR3mNwWK8xSqL2xXRoi_XDMsHA92Zj8-SougcpgkpE0SxW3Yx',
+  },
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
